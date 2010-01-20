571a26960769581ab5fffd53d434d342719b839a
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-01-20 16:45:12 +0100

    plus de gestion js de la langue

--- output/Scratch/js/index.js
+++ output/Scratch/js/index.js
@@ -6,42 +6,6 @@ function detectIE() {
 }
 
 // --- fin pour la contribution de la fin de IE ---
-
-// Cache les div des classes langue non choisie
-// et affiche les div de la classe choisie
-// en gros si on choisi la langue fr et que l'on 
-var currentLanguage="fr";
-var languagesSet=[ 'fr', 'en' ];
-function setLanguage(lang) {
-    currentLanguage=lang;
-    $.cookie('language',lang, { path: '/Scratch'});
-    document.title = 'Yann Esposito - ' + $('#titre h1 .' + lang).text() ;
-    $('.activelang').removeClass("activelang");
-    $('#lang_'+lang).addClass("activelang");
-    $('.'+lang).hide();
-    $('.'+lang).fadeIn();
-    jQuery.each(
-        jQuery.grep(languagesSet, function(a) { return a != lang }),
-        function (i,val) {
-            $('.'+val).hide();
-        }
-    );
-}
-
-function setLanguageFromCookie() {
-    language = $.cookie('language');
-    if (! language) {
-        if ( (navigator) && 
-                (navigator.language) && 
-                (navigator.language.substring(0,2) == 'fr' ) ) {
-            language='fr';
-        } else {
-            language='en';
-        }
-    }
-    setLanguage(language);
-}
-
 function detectiPhone() {
     if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
         $('head').append('<link rel="stylesheet" type="text/css" href="/Scratch/css/iphone.css"/>');
@@ -54,11 +18,12 @@ function detectiPhone() {
     }
 }
 
+
+// initialisation du menu
 var decalageTop = 10;
 function entete_height() {
     return $('#entete').height() - $('#menuMessage').height() + decalageTop;
 }
-
 function hideMenu() {
     $('#content').animate({top: - entete_height()}, 500 );
     $('#menuMessage').html('↓ Menu ↓')
@@ -76,10 +41,7 @@ function toggleMenu() {
         hideMenu();
     }
 }
-
-// initialisation du menu
 var last=0;
-
 function autoHideMenu(value) {
     setTimeout(function(){
         if ( last == value ) {
@@ -87,7 +49,6 @@ function autoHideMenu(value) {
         }
     },5000);
 }
-
 function initMenu() {
     $('#titre').css({top: -entete_height});
     $('#entete').append('<div id="menuMessage">↓ Menu ↓</div>');
@@ -101,7 +62,6 @@ function initMenu() {
 $(document).ready( function() {
     detectIE();
     detectiPhone();
-    setLanguageFromCookie();
     initMenu();
     // affiche la page une fois propre
     $('#blackpage').fadeOut();
