995d7b46f1eabbadeb31fce00f62aab322f25e3e
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-02-17 13:27:01 +0100

    Content added + script for doing that.

--- lib/multiMenu.rb
+++ lib/multiMenu.rb
@@ -5,8 +5,8 @@ def homepage
 end
 
 def sortedChildrenByMenuPriority(item)
-    item.children.reject{|p| p.isHidden}.
-        sort!{|x,y| x.menupriority <=> y.menupriority}
+    item.children.reject{|p| p[:isHidden]}.
+        sort!{|x,y| x[:menupriority] <=> y[:menupriority]}
 end
 
 def generateMenu
@@ -17,4 +17,26 @@ def generateMenu
             link_to_unless_current(page[:title],page.reps[0]) + '</li>'
     end   
     '<ul>'+buffer+'</ul>'
+end
+
+def depthOf(item)
+    puts 'depthOf'
+    return 1
+end
+
+def blogimage(val,title)
+    puts 'blogimage'
+    return '<img alt="pas encore fait" src="/404"></img>'
+end
+def leftblogimage(val,title)
+    puts 'blogimage'
+    return '<img class="left" alt="pas encore fait" src="/404"></img>'
+end
+def tagCloud
+    puts 'tagCloud'
+    return '<div>This should be a TagCloud</div>'
+end
+def lnkto(title,item)
+    puts 'lnkto'
+    return %{<a href="#">#{title}</a>}
 end
---
+++ recupen.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+BEGIN{
+    $/="";
+}
+
+sub replaceminim {
+    my $langue = $_[0] ;
+    my $keep = $_[1] ;
+
+    # protection des X et Y pour qu'ils disparaissent
+    s/X/_wasx_/g;
+    s/Y/_wasy_/g;
+    
+    # remplacement de la chaine en un seul caractère
+    s/$langue/X/g;          # begin
+    s/::/Y/g;               # end
+
+    # A partir de maintenant tous les X sont les begin
+    # tous les Y sont les ends
+    # on veut récupérer ce qu'il y a à l'intérieur
+    if ($keep) {
+        s/X(([^XY]|\n)*?)Y/$1/gm;   # suppression begin..end
+    } else {
+        s/X([^XY]|\n)*?Y//gm;   # suppression begin..end
+    }
+    
+    # recuperation des X, Y, et begin et end non traites
+    s/X/$langue/g;
+    s/Y/::/g;
+    s/_wasx_/X/g;
+    s/_wasy_/Y/g;
+}
+replaceminim( "en:", 1);
+replaceminim( "fr:", 0);
