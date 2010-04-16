04eb99e0571febe096b61b38425f07f8ff109a1a
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-04-16 16:31:56 +0200

    Nice first page for my blog post.

---
+++ lib/repair_html.rb
@@ -0,0 +1,26 @@
+# All files in the 'lib' directory will be loaded
+# before nanoc starts compiling.
+
+# repair cutted XML code by closing the tags
+# work even if the XML is cut into a tag.
+# example:
+#    transform '<div> <code> toto </code> <p> hello <a href="http://tur'
+#    into      '<div> <code> toto </code> <p> hello </p></div>'
+def repair_html( html )
+    parents=[]
+    depth=0
+    html.scan( %r{<(/?)(\w*)} ).each do |m|
+        if m[0] == "" 
+            parents[depth]=m[1]
+            depth+=1
+            puts '<'+m[0]+m[1]+'> ' + depth.to_s
+            puts 'parents='+parents.join('; ')
+        else
+            depth-=1
+            puts '<'+m[0]+m[1]+'> ' + depth.to_s
+        end
+    end
+    res=html.sub(/<[^>]*$/m,'')
+    depth.downto(0).each { |x| res<<= %{</#{parents[x]}>} }
+    res
+end
