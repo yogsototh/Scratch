419cc6cc7375b056743e5aefda566ce5ba0f09fa
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-04-19 09:01:09 +0200

    Changed directory name

--- tasks/serve.py
+++ tasks/serve.py
@@ -3,7 +3,7 @@ import os # pour le chdir
 import SimpleHTTPServer
 import SocketServer
 # minimal web server.
-os.chdir(os.environ['HOME']+'/Sites/n3blog/output')
+os.chdir(os.environ['HOME']+'/Sites/webroot/output')
 PORT = 8000
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 httpd = SocketServer.TCPServer(("", PORT), Handler)
