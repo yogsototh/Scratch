274a4af3c1419798b24dd85ec009c6b47eeba27c
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-05-20 18:01:37 +0200

    I prefer like this

--- output/Scratch/js/index.js
+++ output/Scratch/js/index.js
@@ -162,21 +162,8 @@ $(document).ready( function() {
     if ( alertLanguage() ) {
         $('#blackpage').fadeOut();
     }
-});
 
-
-
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-10612400-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
- var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
- ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
- var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
- })();
-
-$(function () {
+    // add an event to all link for google analytics
     $('a').click(function () {
         // tell analytics to save event
         try {
@@ -192,3 +179,16 @@ $(function () {
         } while(curDate-date < 300);
     });
 });
+
+
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-10612400-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+ var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+ var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+ })();
+
