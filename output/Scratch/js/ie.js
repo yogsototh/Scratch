49f506a9cf96c0c89a6d266cab106e7d76ef54e0
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-06-07 11:23:17 +0200

    IE handling

--- output/Scratch/js/ie.js
+++ output/Scratch/js/ie.js
@@ -2,8 +2,12 @@ $('link[rel=stylesheet]').each(function(i)
 {
     initMenu=function(){};
     // if (this.getAttribute('href') == '/Scratch/assets/css/layout.css') this.disabled = true;
-    if (this.getAttribute('href') == '/Scratch/css/shadows.css') this.disabled = true;
-    if (this.getAttribute('href') == '/Scratch/assets/css/gen.css')    this.disabled = true;
+    if (this.getAttribute('href') == '/Scratch/css/shadows.css') { 
+        this.disabled = true;
+    }
+    // if (this.getAttribute('href') == '/Scratch/assets/css/gen.css') {
+    //     this.disabled = true;
+    // }
 }) ;
 $('head').append('<link rel="stylesheet" type="text/css" href="/Scratch/css/ie.css"/>');
-$('#titre').prepend('<div id="iemessage"><p><span><em>Avec <a href="http://www.firefox.com"> Firefox </a> et <a href="http://www.apple.com/safari">Safari</a> cette page est bien plus jolie !</em></span><br/><span><em>This page is far nicer with <a href="http://www.firefox.com"> Firefox </a> and <a href="http://www.apple.com/safari">Safari</a>!</em></span></p></div>');
+$('body').prepend('<div id="iemessage"><p><span><em>Avec <a href="http://www.firefox.com"> Firefox </a> et <a href="http://www.apple.com/safari">Safari</a> cette page est bien plus jolie !</em></span><br/><span><em>This page is far nicer with <a href="http://www.firefox.com"> Firefox </a> and <a href="http://www.apple.com/safari">Safari</a>!</em></span></p></div>');
