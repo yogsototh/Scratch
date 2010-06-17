3a0b26b5cc31ac7b8a786429fa8363b9418e15bd
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-06-17 16:34:33 +0200

    Ability to hide to analytics (not counting myself)

--- output/Scratch/js/index.js
+++ output/Scratch/js/index.js
@@ -155,50 +155,60 @@ $(document).ready( function() {
         $('#blackpage').fadeOut();
     }
 
-    // add an event to all link for google analytics
-    $('a').click(function () {
-        // tell analytics to save event
-        try {
-            var identifier=$(this).attr('id') ;
-            var href=$(this).attr('href')
-            var label="";
-            if ( typeof( identifier ) != 'undefined' ) {
-                label=label+'[id]:'+identifier
-                category='JSLink'
-            }
-            if ( typeof( href ) != 'undefined' ) {
-                label=label+' [href]:'+href
-                if ( href[0] == '#' ) {
-                    category='Anchor';
-                } else {
-                    category='Link';
+    var admin = $.cookie('admin');
+    if (! admin) {
+        // console.log("you're logged by google analytics");
+        // add an event to all link for google analytics
+        $('a').click(function () {
+            // tell analytics to save event
+            try {
+                var identifier=$(this).attr('id') ;
+                var href=$(this).attr('href')
+                var label="";
+                if ( typeof( identifier ) != 'undefined' ) {
+                    label=label+'[id]:'+identifier
+                    category='JSLink'
                 }
+                if ( typeof( href ) != 'undefined' ) {
+                    label=label+' [href]:'+href
+                    if ( href[0] == '#' ) {
+                        category='Anchor';
+                    } else {
+                        category='Link';
+                    }
+                }
+                _gaq.push(['_trackEvent', category, 'clicked', label]);
+                // console.log('[tracked]: ' + category + ' ; clicked ; ' + label );
             }
-            _gaq.push(['_trackEvent', category, 'clicked', label]);
-            // console.log('[tracked]: ' + category + ' ; clicked ; ' + label );
-        }
-        catch (err) {
-            console.log(err);
-        }
+            catch (err) {
+                console.log(err);
+            }
 
-        // pause to allow google script to run
-        var date = new Date();
-        var curDate = null;
-        do {
-            curDate = new Date();
-        } while(curDate-date < 300);
-    });
+            // pause to allow google script to run
+            var date = new Date();
+            var curDate = null;
+            do {
+                curDate = new Date();
+            } while(curDate-date < 300);
+        });
+    } else {
+        console.log("[WARNING] you're HIDDEN to analytics");
+    }
 });
 
 
 
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-10612400-1']);
-_gaq.push(['_trackPageview']);
+if ( ! $.cookie('admin') ) {
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-10612400-1']);
+    _gaq.push(['_trackPageview']);
 
-(function() {
- var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
- ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
- var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
- })();
+    (function() {
+     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+     })();
+} else {
+    console.log("[WARNING] you're HIDDEN to analytics");
+}
 
