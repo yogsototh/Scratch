f8dce07adf7ba69f5bce8ac068cb79b16931f8ea
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-06-23 16:59:32 +0200

    New layout for articles

--- output/Scratch/js/index.js
+++ output/Scratch/js/index.js
@@ -113,48 +113,6 @@ function detectiPhone() {
 }
 
 
-// initialisation du menu
-var decalageTop = 10;
-function entete_height() {
-    return $('#entete').height() - $('#menuMessage').height() + decalageTop;
-}
-function fastHideMenu() {
-    $('#entete').css({opacity: .2});
-}
-function hideMenu() {
-    $('#entete').animate({opacity: .2}, 3000 );
-}
-function showMenu() {
-    $('#entete').animate({opacity:1}, 200 );
-    last+=1;
-    autoHideMenu(last);
-}
-function fastShowMenu() {
-    $('#entete').css({opacity:1});
-    last+=1;
-    autoHideMenu(last);
-}
-function toggleMenu() {
-    if ( $('#entete').css('top')==entete_height() ) {
-        showMenu();
-    } else {
-        hideMenu();
-    }
-}
-var last=0;
-function autoHideMenu(value) {
-    setTimeout(function(){
-        if ( last == value ) {
-            hideMenu();
-        }
-    },2000);
-}
-function initMenu() {
-    $('#titre').css({top: -entete_height});
-    $('#entete').hover(showMenu);
-    $('#entete').hover(function(){last+=1;}, function(){autoHideMenu(last);});
-    autoHideMenu(0);
-}
 
 function setSpecificCss() {
     var userAgent = navigator.userAgent.toLowerCase();
@@ -171,7 +129,6 @@ $(document).ready( function() {
     setSpecificCss();
     detectIE();
     detectiPhone();
-    initMenu();
     initCode();
     // affiche la page une fois propre et la langue choisie
     if ( alertLanguage() ) {
