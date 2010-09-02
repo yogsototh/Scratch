65eb7148522eac617e58b5622a9b4761620971e0
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-09-02 11:40:24 +0200

    Optimized gitmtime using Eric Sunshine advices

--- lib/gitmtime.rb
+++ lib/gitmtime.rb
@@ -1,18 +1,7 @@
 def gitmtime
-    # find file extension
     filepath=@item.path.sub('/Scratch/','content/html/').sub(/\/$/,'')
-    ['md','erb','haml'].each do |ext|
-        tmppath=%{#{filepath}.#{ext}}
-        if FileTest.exists?(tmppath)
-            filepath=tmppath
-            break
-        end
-    end
-    str=`git log --format='%ci' -- #{filepath} | head -n 1`
-    # puts '#############'
-    # puts "item #{filepath}"
-    # puts "str #{str}"
-    # puts '#############'
+    filepath<<=%{.#{@item[:extension]}}
+    str=`git log -1 --format='%ci' -- #{filepath}`
     if str == ""
         return @item.mtime
     else
