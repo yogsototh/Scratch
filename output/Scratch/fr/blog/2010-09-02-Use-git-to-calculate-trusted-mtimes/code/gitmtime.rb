6c6d9749207477b0059ffaee8321fecffe8a1b58
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-09-02 16:51:37 +0200

    New blog entry

--- output/Scratch/fr/blog/2010-09-02-Use-git-to-calculate-trusted-mtimes/code/gitmtime.rb
+++ output/Scratch/fr/blog/2010-09-02-Use-git-to-calculate-trusted-mtimes/code/gitmtime.rb
@@ -7,8 +7,8 @@ def gitmtime
         filepath.sub!(ext,%{/index#{ext}})
     end
     str=`git log -1 --format='%ci' -- #{filepath}`
-    if str == ""
-        return @item.mtime
+    if str.nil? or str.empty?
+        return Time.now
     else
         return DateTime.parse( str )
     end
