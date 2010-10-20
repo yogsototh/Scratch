adbe7996216087f1e47f27b53189912c52edb1d2
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-10-20 17:20:28 +0200

    added macros support %toto

--- lib/macros.rb
+++ lib/macros.rb
@@ -1,3 +1,32 @@
+class Macros < Nanoc3::Filter
+    identifier :falacy
+    attr_accessor :macro
+    def initialize(arg)
+        super
+        @macro={}
+        @macro[:tlal] = %{<span class="sc"><abbr title="Trop long à lire">tlàl</abbr> : </span>}
+        @macro[:tldr] = %{<span class="sc"><abbr title="Too long; don't read">tl;dr</abbr>: </span>}
+        if not $macros.nil?
+            @macro.merge!( $macros )
+        end
+    end
+    def macro_value_for(macro_name)
+        if macro_name.nil? or macro_name=="" or @macro[macro_name.intern].nil?
+            return %{%#{macro_name}} 
+        end
+        return @macro[macro_name.intern]
+    end
+    def run(content, params={})
+        content.gsub(/%(\w*)/) do |m| 
+            if m != '%'
+                macro_value_for($1)
+            else
+                m
+            end
+        end
+    end
+end
+
 def tldr
     %{<span class="sc"><abbr title="Too long; don't read">tl;dr</abbr>: </span>}
 end
@@ -5,3 +34,4 @@ end
 def tlal
     %{<span class="sc"><abbr title="Trop long à lire">tlàl</abbr> : </span>}
 end
+
