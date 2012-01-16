c58de1e79f54658a1ef0ba807d477a03244a53e5
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2012-01-16 22:02:35 +0100

    Real script to cleanup all blog entries

--- tasks/devel-clean.sh
+++ tasks/devel-clean.sh
@@ -5,11 +5,14 @@ print $webroot
 print -- "Clean content's blog"
 for fic in $webroot/content/html/??/blog/*(N); do
     print "[d] $fic"
-    rm -rf $fic
+    \rm -rf $fic
 done
 print -- "Clean multi/blog"
-for fic in $(print -l -- $webroot/multi/blog/*(N) | head -n -4); do
+list=$(ls -lt -- $webroot/multi/blog | awk '{print $9}')
+nb=$(( $( print -l $list | wc -l ) - 4 ))
+res=( $( print -- $list | tail -n -$nb ) )
+for fic in $res; do
     print "[d] $fic"
-    \rm -rf $fic
+    \rm -rf $webroot/multi/blog/$fic
 done
 
