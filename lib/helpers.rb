0fb10448a050a669a6ca107761b8a3b69795594b
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2012-04-17 11:00:23 +0200

    Typos 3

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -9,7 +9,7 @@
 # <% end %>
 # Pour remplir à la fois le texte et le sommaire
 
-include Nanoc3::Helpers::Capturing
+# include Nanoc3::Helpers::Capturing
 include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::Blogging
 include Nanoc3::Helpers::Text
